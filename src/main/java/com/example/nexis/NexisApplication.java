0b95c1cf08233a4dab55a9029b90137890582f9b
Trần Đông Đông <dongtd2112@gmail.com>
2025-01-02 22:56:17 +0700

    Deploy8

--- src/main/java/com/example/nexis/NexisApplication.java
+++ src/main/java/com/example/nexis/NexisApplication.java
@@ -8,20 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NexisApplication {
 
 	public static void main(String[] args) {
-		String databaseUrl = DotenvConfig.get("DATASOURCE_URL");
-		String databaseUsername = DotenvConfig.get("DATASOURCE_USERNAME");
-		String databasePassword = DotenvConfig.get("DATASOURCE_PASSWORD");
-
-		String mailUsername = DotenvConfig.get("MAIL_USERNAME");
-		String mailPassword = DotenvConfig.get("MAIL_PASSWORD");
-
-		// Đặt giá trị vào System Properties
-		System.setProperty("spring.datasource.url", databaseUrl);
-		System.setProperty("spring.datasource.username", databaseUsername);
-		System.setProperty("spring.datasource.password", databasePassword);
-
-		System.setProperty("spring.mail.username", mailUsername);
-		System.setProperty("spring.mail.password", mailPassword);
+//		String databaseUrl = DotenvConfig.get("DATASOURCE_URL");
+//		String databaseUsername = DotenvConfig.get("DATASOURCE_USERNAME");
+//		String databasePassword = DotenvConfig.get("DATASOURCE_PASSWORD");
+//
+//		String mailUsername = DotenvConfig.get("MAIL_USERNAME");
+//		String mailPassword = DotenvConfig.get("MAIL_PASSWORD");
+//
+//		// Đặt giá trị vào System Properties
+//		System.setProperty("spring.datasource.url", databaseUrl);
+//		System.setProperty("spring.datasource.username", databaseUsername);
+//		System.setProperty("spring.datasource.password", databasePassword);
+//
+//		System.setProperty("spring.mail.username", mailUsername);
+//		System.setProperty("spring.mail.password", mailPassword);
 
 		SpringApplication.run(NexisApplication.class, args);
 		System.out.println("Nexis Application is running...");
