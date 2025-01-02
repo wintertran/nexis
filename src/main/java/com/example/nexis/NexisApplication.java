cb129467ab93a95a0d9e8c52ea662d93a83d8f04
Trần Đông Đông <dongtd2112@gmail.com>
2025-01-02 22:27:11 +0700

    Deploy8

--- src/main/java/com/example/nexis/NexisApplication.java
+++ src/main/java/com/example/nexis/NexisApplication.java
@@ -1,5 +1,6 @@
 package com.example.nexis;
 
+import com.example.nexis.config.DotenvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NexisApplication {
 
 	public static void main(String[] args) {
+		String databaseUrl = DotenvConfig.get("DATASOURCE_URL");
+		String databaseUsername = DotenvConfig.get("DATASOURCE_USERNAME");
+		String databasePassword = DotenvConfig.get("DATASOURCE_PASSWORD");
+
+		String mailUsername = DotenvConfig.get("MAIL_USERNAME");
+		String mailPassword = DotenvConfig.get("MAIL_PASSWORD");
+
+		// Đặt giá trị vào System Properties
+		System.setProperty("spring.datasource.url", databaseUrl);
+		System.setProperty("spring.datasource.username", databaseUsername);
+		System.setProperty("spring.datasource.password", databasePassword);
+
+		System.setProperty("spring.mail.username", mailUsername);
+		System.setProperty("spring.mail.password", mailPassword);
+
 		SpringApplication.run(NexisApplication.class, args);
 		System.out.println("Nexis Application is running...");
 	}
---
+++ src/main/java/com/example/nexis/config/DotenvConfig.java
@@ -0,0 +1,11 @@
+package com.example.nexis.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DotenvConfig {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    public static String get(String key) {
+        return dotenv.get(key);
+    }
+}
